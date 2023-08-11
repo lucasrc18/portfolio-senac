c4f20bb9804391c0c22f31570b95daa44a39ea58
lucasrc18 <lucas.rcaetano1122@gmail.com>
2023-08-11 03:06:17 -0300

    Finalizando projetos

---
+++ projetos/landingpage/scripts/main.js
@@ -0,0 +1,117 @@
+const highlights = [
+    "E-Commerce",
+    "Portfolio",
+    "Cadastro",
+    "Dashboard",
+    "Blog"
+]
+
+const images = [
+    "../assets/Internet.svg",
+    "../assets/Login.svg",
+    "../assets/Dashboard.svg",
+    "../assets/Portfolio.svg",
+    "../assets/Bloging.svg"
+]
+
+const steps_ref = document.getElementsByClassName("step");
+
+let step = 0;
+function update_step(){
+    if(!steps_ref[step].classList.contains("current"))
+        steps_ref[step].classList.add("current")
+
+    for(let i = 0; i < steps_ref.length; i++){
+        if(step != i){
+            if(steps_ref[i].classList.contains("current"))
+                steps_ref[i].classList.remove("current")
+        }
+    }
+
+
+
+    setTimeout(update_step, 2500)
+    
+    if(step + 1 <= 2)
+        step += 1
+    else   
+        step = 0
+}
+
+let cur = 0, letter = -1;
+function typing_effect(){
+    const highlight = document.getElementById("highlight")
+    const current = highlights[cur] + "       "
+    
+    letter += 1
+    if(letter <= current.length-1)
+        highlight.innerHTML = current.substring(0, letter)
+    else {
+        highlight.innerHTML = ""
+        cur += 1
+        letter = -1
+
+        if(cur >= highlights.length)
+            cur = 0
+	
+	document.getElementById("page-image").src = images[cur]
+    }
+
+    setTimeout(typing_effect, 250)
+}
+
+window.onload = function(){
+    update_step()
+    typing_effect()
+}
+
+document.getElementById("showme-btn").addEventListener("click", () => {
+    window.location.href = "/projetos/produtos/index.html"
+})
+
+document.getElementById("login-btn").addEventListener("click", () => {
+    window.location.href = "/projetos/login/index.html"
+})
+
+document.querySelectorAll("footer section").forEach(e => {
+    e.addEventListener("click", () => {
+        if(!e.classList.contains("current"))
+            e.classList.add("current")
+
+        const num = e.id.charAt(1)
+        if(num == 1){
+            document.getElementById('s2').classList.remove("current")
+            document.getElementById('s3').classList.remove("current")
+        } else if (num == 2) {
+            document.getElementById('s1').classList.remove("current")
+            document.getElementById('s3').classList.remove("current")
+        } else {
+            document.getElementById('s1').classList.remove("current")
+            document.getElementById('s2').classList.remove("current")
+        }
+
+        step = num - 1
+    })
+})
+
+for(let i = 0; i < 4; i++){
+    document.getElementById("nav-item-" + (i+1)).addEventListener("click", () => {
+        nav_functions[i]()
+    })
+}
+
+const nav_functions = [
+    function(){
+        window.location.reload()
+    },
+    function(){
+        window.alert("Entre em contato pelo email: lucas.rcaetano1122@gmail.com")
+    },
+    function(){
+        window.location.href = "/projetos/produtos/index.html"
+    },
+    function(){
+        window.location = window.location.origin
+    }
+]
+    
---
+++ projetos/produtos/scripts/main.js
@@ -0,0 +1,99 @@
+const img = {
+    Blog: "../assets/Bloging.svg",
+    ECommerce: "../assets/Internet.svg",
+    Portfolio: "../assets/Portfolio.svg",
+    SignUp: "../assets/Login.svg",
+    Dashboard: "../assets/Dashboard.svg"
+}
+
+const products = [
+    {
+        image: img.Blog,
+        page: "Blog",
+        price: "1199,99",
+        desc: "Um blog para discutir dos mais variados temas!"
+    },
+    {
+        image: img.ECommerce,
+        page: "E-commerce",
+        price: "1599,99",
+        desc: "Realize seu negócio online!"
+    },
+    {
+        image: img.Portfolio,
+        page: "Portfolio",
+        price: "1599,99",
+        desc: "Registre seus trabalhos, e faça seu nome conhecido!"
+    },
+    {
+        image: img.SignUp,
+        page: "Cadastro",
+        price: "800,00",
+        desc: "Uma página de cadastro para acomodar seus clientes!"
+    },
+    {
+        image: img.Dashboard,
+        page: "Dashboard",
+        price: "999,99",
+        desc: "Acompanhe o andamento de seu negócio!"
+    }
+]
+
+const ref = {
+    image: document.querySelector("#product_image"),
+    name: document.querySelector("#product_name"),
+    desc: document.querySelector("#product_description"),
+    price: document.querySelector("#product_price"),
+
+    left_arrow: document.querySelector("#left_arrow"),
+    right_arrow: document.querySelector("#right_arrow")
+}
+
+// Handlers
+ref.left_arrow.addEventListener("click", () => {
+    if(cur - 1 < 0) {
+        cur = products.length - 1
+    } else {
+        cur -= 1
+    }
+
+    updateProduct()
+})
+ref.right_arrow.addEventListener("click", () => {
+    if(cur + 1 > products.length - 1) {
+        cur = 0
+    } else {
+        cur += 1
+    }
+
+    updateProduct()
+})
+
+// Animations
+ref.left_arrow.addEventListener("mouseover", () => {
+    ref.left_arrow.src = "../assets/filled_left_arrow.svg"
+})
+ref.left_arrow.addEventListener("mouseout", () => {
+    ref.left_arrow.src = "../assets/left_arrow.svg"
+})
+
+ref.right_arrow.addEventListener("mouseover", () => {
+    ref.right_arrow.src = "../assets/filled_right_arrow.svg"
+})
+ref.right_arrow.addEventListener("mouseout", () => {
+    ref.right_arrow.src = "../assets/right_arrow.svg"
+})
+
+let cur = 0
+function updateProduct(){
+    ref.name.innerHTML = products[cur].page
+    ref.desc.innerHTML = products[cur].desc
+    ref.price.innerHTML = products[cur].price
+    ref.image.src = products[cur].image
+}
+
+window.onload = updateProduct
+
+document.querySelector("button").addEventListener("click", () => {
+    alert("Compra efetuada com sucesso!")
+})
